d1415c5a26fce4bcd61522116c6b00e39015516a
Evgeniy Kabirov <probuborka@gmail.com>
2024-06-30 21:03:34 +0300

    second iteration

--- main.go
+++ main.go
@@ -34,6 +34,9 @@ func (t Training) distance() float64 {
 // meanSpeed возвращает среднюю скорость бега или ходьбы.
 func (t Training) meanSpeed() float64 {
 	// вставьте ваш код ниже
+	if t.Duration == 0 {
+		return 0
+	}
 	return t.distance() / t.Duration.Hours()
 }
 
